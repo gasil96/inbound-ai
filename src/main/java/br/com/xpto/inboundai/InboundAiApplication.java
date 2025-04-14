2023dc5ae7a755f7a6e2287ce25d3e489fc8d4e4
Gabriel Silva <>
2025-04-14 17:37:16 -0300

    commit

---
+++ src/main/java/br/com/xpto/inboundai/InboundAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.xpto.inboundai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboundAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboundAiApplication.class, args);
+	}
+
+}
